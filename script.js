79390bc4ab302131b8942aad552e3c2e2f7ffd9f
Bhaskar M Gupta <119575462+bhaskarmgupta@users.noreply.github.com>
2024-04-08 09:02:18 +0530

    third commit

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["a Learner","a Freelancer","Developer"],
+    strings : ["a Learner","a Freelancer","a Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
